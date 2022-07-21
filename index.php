14e1ea656f254a833bdef68312f8aaec7e2b62d0
taedium8vitae <gabguinn@bluewin.ch>
2022-07-21 23:23:30 +0200

    remade the front page

--- index.php
+++ index.php
@@ -3,10 +3,14 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Work+Sans:800,700,500,400">
         <link href="/styles.css" rel="stylesheet" type="text/css">
         <title>CSM Cinéclub</title>
     </head>
+
     <?php
+    // Connexion à la base de données
+
     try{
         $bdd = new PDO('mysql:host=localhost;dbname=cinema;charset=utf8', 'root', 'root');
     }
@@ -18,27 +22,72 @@
     $moviesStatement->execute();
     $movies = $moviesStatement->fetchAll();
     ?>
-    </head>
+
     <body>
-        <header class="head">
-            <h1>Cinéclub du collège St-Michel</h1>
+        <header class="primary-header">
+            <div class="container">
+                <div class="logo">
+                    <a href="/">CSM Cinéclub</a>
+                </div>
+                <nav class="primary-navigation">
+                    <ul role="list" class="nav-list">
+                        <li><a href="#">Films</a></li>
+                        <li><a href="#">A Propos</a></li>
+                    </ul>
+                </nav>
+            </div>
         </header>
-        <div class="media_scroller snaps-inline">
-            <?php
-            $today = date("Y-m-d");
-            foreach(array_reverse($movies) as $movie){
-                ?>
-                    <div class="movie<?php if($today <= $movie['date']){echo ' future';} ?>">
-                        <img class="poster" src="/images/<?php echo $movie['linkToImage']; ?>" alt="<?php echo $movie['title']; ?>">
-                        <h2><?php echo $movie['title']; ?></h2>
-                        <p class="date"><?php echo substr($movie['date'], 0, -3); ?></p>
-                    </div>
-                <?php
-                }
-                ?>
-        </div>
+
+        <main>
+            <div class="nextMovie">
+                <div class="poster">
+                    <?php
+                    // Vérifier si le prochain film a été ajouté à la base de données
+                    $nextMovieAnnounced = false;
+                    foreach ($movies as $movie) {
+                        if ($movie['date'] > date('Y-m-d')) {
+                            $nextMovieAnnounced = true;
+                            break;
+                        }
+                    }
+                    // Mettre la variable $movie sur le prochain film
+                    if ($nextMovieAnnounced == true) {
+                        foreach ($movies as $movie) {
+                            if ($movie['date'] > date('Y-m-d')) {
+                                break;
+                            }
+                        }
+                        // Afficher le prochain film
+                        ?>
+                        <img src="/images/<?php echo $movie['linkToImage'] ?>" alt="<?php echo $movie['title'] ?>">
+                        <h2 class="movieTitle"><?php echo $movie['title'] ?></h2>
+                        <h2 class="movieDate"><?php echo date('d M Y',strtotime($movie['date'])); ?></h2>
+                        <?php
+                    } else {
+                        // Afficher que le prochain film n'a pas encore été annoncé
+                        ?>
+                        <img src="/images/noMovie.png" alt="Le prochain film n'a pas encore été annoncé">
+                        <h2 class="movieTitle">Aucun film à l'affiche</h2>
+                        <?php
+                    }
+                    ?>
+                </div>
+                <div class="paragraph-container">
+                    <p class="synopsis">
+                        <?php
+                        // Afficher le synopsis du prochain film
+                        if ($nextMovieAnnounced == true) {
+                            echo $movie['text'];
+                        } else {
+                            echo "Le prochain film n'a pas encore été annoncé, revenez plus tard ! :)";
+                        }
+                        ?>
+                    </p>
+                </div>
+            </div>
+        </main>
         <footer>
-            <p class="foot">&copy; Copyright 2022 - Cinéclub du collège St-Michel</p>
+            <p>&copy; Copyright 2022 - Cinéclub du collège St-Michel</p>
         </footer>
     </body>
 </html>
