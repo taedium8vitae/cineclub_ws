cd00a5d690789f0a407a6ba5eff29d711857f7de
taedium8vitae <gabguinn@bluewin.ch>
2022-07-29 22:01:51 +0200

    links to socials and css work

--- about/index.php
+++ about/index.php
@@ -13,7 +13,11 @@
             <div class="about-content">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             </div>
-            <div id="map"></div>
-            </div>
+            <img src="" alt="Plan du collège St-Michel">
+        </div>
+        <div class="social">
+            <div class="social-div"><a href="https://www.instagram.com/csm_cineclub/" class="social-text">Instagram</a></div>
+            <div class="social-div"><a href = "mailto: xxxxxxxx" class="social-text">Email</a></div>
+        </div>
         <?php include('../footer.php'); ?>
     </body>
