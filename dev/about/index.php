a5ee6c13b8fa43cc32aecf380c05896cbf0a253d
taedium8vitae <gabguinn@bluewin.ch>
2022-08-10 02:00:58 +0200

    spelling mistake

--- dev/about/index.php
+++ dev/about/index.php
@@ -11,7 +11,7 @@
         <?php include('../header.php'); ?>
         <div class="about-container">
             <div class="about-content">
-            Le Cinéclub du Collège St-Michel, c’est un groupe d'étudiants super intéressés par le cinéma. Nous organisons normalement toutes les deux semaines des séances de films, avec une programmation que nous essayons de rendre le plus varié possible. Les projections, ouvertes à tous les étudiants, ont lieu dans l'Aula du Collège St-Michel à Fribourg. L’entrée est toujours gratuite.
+            Le Cinéclub du Collège St-Michel, c’est un groupe d'étudiants super intéressés par le cinéma. Nous organisons normalement toutes les deux semaines des séances de films, avec une programmation que nous essayons de rendre la plus variée possible. Les projections, ouvertes à tous les étudiants, ont lieu dans l'Aula du Collège St-Michel à Fribourg. L’entrée est toujours gratuite.
             Pour plus d'informations, diverses questions, ou encore pour toute suggestion, n'hésitez pas à nous contacter par Mail ou sur notre compte Instagram.
             </div>
             <!--<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d682.7228138883139!2d7.156460146316475!3d46.80644795460093!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x478e6931cfb01ce9%3A0x842301919f05fd50!2sRue%20Saint-Pierre%20Canisius%2C%201700%20Fribourg!5e0!3m2!1sfr!2sch!4v1659621867255!5m2!1sfr!2sch" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>-->
