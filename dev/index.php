aa37432a1bb924eca106eb5a47dd21ce3f11d1de
taedium8vitae <gabguinn@bluewin.ch>
2022-11-30 18:28:01 +0100

    Adding message preview support
    You can now preview the site right in your messages as an embed!

--- dev/index.php
+++ dev/index.php
@@ -1,49 +1,5 @@
-<!doctype html>
-<!--
-.........................................................
-.........................................................
-....................*****************....................
-...............******...............******...............
-...........****...........................****...........
-.........****.................................***........
-......***.......................................***......
-.....**...........***...............***...........**.....
-...**...........*******...........*******..........***...
-..**............*******...........*******............**..
-.**.............*******...........*******.............**.
-.**...............***...............***...............**.
-**.....................................................**
-**.......*.....................................*.......**
-**......**.....................................**......**
-.**...****.....................................****...**.
-.**......**...................................**......**.
-..**.......***.............................***.......**..
-...***.......****.......................****.......***...
-.....**.........******.............******.........**.....
-......***............***************............***......
-........****.................................****........
-...........****...........................****...........
-...............******...............******...............
-....................*****************....................
-.........................................................
--->
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Work+Sans:800,700,500,400">
-        <link href="/styles.css" rel="stylesheet" type="text/css">
-        <title>Cinéclub du Collège St-Michel Fribourg</title>
-        <meta name="description" content="Découvrez le prochain film à venir au Cinéclub du collège St-Michel Fribourg." />
-    </head>
-
-    <?php include('dbConnect.php'); ?>
-
-    <body>
-        <?php include('header.php'); ?>
-        <main>
-            <div class="nextMovie">
-                <div class="poster">
-                    <?php
+<?php include('dbConnect.php'); ?>
+<?php
                     // Vérifier si le prochain film a été ajouté à la base de données
                     $nextMovieAnnounced = false;
                     foreach ($movies as $movie) {
@@ -59,9 +15,50 @@
                                 break;
                             }
                         }
+                    }
+?>
+<!doctype html>
+<html>
+<head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Work+Sans:800,700,500,400">
+        <link href="/styles.css" rel="stylesheet" type="text/css">
+        <title>Cinéclub du Collège St-Michel Fribourg</title>
+        <meta name="description" content="Découvrez le prochain film à venir au Cinéclub du collège St-Michel Fribourg." />
+        <meta name="og:title" content="<?php 
+            if ($nextMovieAnnounced == true) {
+                echo $movie['title'];
+            }else{
+                echo "CSM Cinéclub";
+            }
+            ?>" />
+        <meta name="og:description" content="
+            <?php 
+            if ($nextMovieAnnounced == true) {
+                echo $movie['text'];
+            }else{
+                echo "Le prochain film n'a pas encore été annoncé, revenez bientôt !";
+            }
+            ?>
+            " />
+        <?php
+            if ($nextMovieAnnounced == true) {
+                echo '<meta name="og:image" content="https://raw.githubusercontent.com/taedium8vitae/cineclub_ws/main/images-mini/' . $movie['linkToImage'] . '" />';
+            }
+        ?>
+    </head>
+
+    <body>
+        <?php include('header.php'); ?>
+        <main>
+            <div class="nextMovie">
+                <div class="poster">
+                    <?php
+                        if ($nextMovieAnnounced == true) {
                         // Afficher le prochain film
                         ?>
-                        <img src="https://raw.githubusercontent.com/taedium8vitae/cineclub_ws/main/images-mini/<?php echo $movie['linkToImage'] ?>" alt="<?php echo $movie['title'] ?>">
+                        <img src="https://raw.githubusercontent.com/taedium8vitae/cineclub_ws/main/images/<?php echo $movie['linkToImage'] ?>" alt="<?php echo $movie['title'] ?>">
                         <h2 class="movieTitle"><?php echo $movie['title'] ?></h2>
                         <h2 class="movieDate"><?php echo date('d M Y',strtotime($movie['date'])); ?></h2>
                         <?php
